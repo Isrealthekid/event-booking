cb9ea5b440d3bb8b892bf6381501132b8392ae29
isrealthekid <oluwoleisreal12@gmail.com>
2024-08-27 11:42:52 +0100

    Footer

--- components/ui/shared/Footer.tsx
+++ components/ui/shared/Footer.tsx
@@ -1,8 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='border-t'>
+      <div className="flex-center wrapper flex-between flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href='/'>
+          <Image 
+            src='/assets/images/logo.svg'
+            alt='Logo'
+            height={38}
+            width={128}
+          />
+        </Link>
+
+        <p>2024 Evently. All rights reserved &copy;</p>
+      </div>
+
+    </footer>
   )
 }
 
