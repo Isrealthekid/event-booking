ed59986a1a2669dffc30ebaac39858f410465a22
isrealthekid <oluwoleisreal12@gmail.com>
2024-08-28 08:52:31 +0100

    types

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -60,8 +60,8 @@ export async function POST(req: Request) {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name!,
-      lastName: last_name!,
+      firstName: first_name,
+      lastName: last_name,
       photo: image_url,
     }
 
